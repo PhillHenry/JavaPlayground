9e0e513ccc336913084df7f60a217956124ccefd
PhillipHenry <PhillHenry@gmail.com>
2023-12-20 16:24:26 +0000

    Richer data

--- src/main/java/uk/co/odinconsultants/validation/Records.java
+++ src/main/java/uk/co/odinconsultants/validation/Records.java
@@ -1,12 +1,12 @@
 package uk.co.odinconsultants.validation;
 
 import jakarta.validation.constraints.NotNull; // seems javax -> jakarta
-record SomeData(@NotNull(message = "Data not be null") String data) {}
+record SomeData(@NotNull(message = "Data not be null") String data, int anInt) {}
 
 public class Records {
 
-    public static void main() {
-        System.out.println("Hello, world");
+    public static void main(String[] args) {
+        System.out.println(new SomeData("some data", 42));
     }
 
 }
