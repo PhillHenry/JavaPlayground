d437b1c0572fa18d74bb37dac24ee446a1ac1db0
PhillipHenry <PhillHenry@gmail.com>
2024-01-16 14:16:55 +0000

    Currying.

--- src/main/java/uk/co/odinconsultants/functions/FunctionsMain.java
+++ src/main/java/uk/co/odinconsultants/functions/FunctionsMain.java
@@ -8,6 +8,10 @@ public class FunctionsMain {
         System.out.println("myMethod: " + x);
         return x + "x";
     }
+    public String myMethod(String x, String y) {
+        System.out.println("Currying myMethod: " + x + ", " + y);
+        return x + "x";
+    }
 
     public void doFunction(Consumer<String> fn) {
         fn.accept("test");
@@ -16,6 +20,7 @@ public class FunctionsMain {
     public static void main(String[] args) {
         var app = new FunctionsMain();
         app.doFunction(app::myMethod);
+        app.doFunction(b -> app.myMethod("curry", b));
     }
 
 }
