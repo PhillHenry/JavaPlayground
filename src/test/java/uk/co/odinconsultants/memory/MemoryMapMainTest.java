cc925a85d19a3737e174a6578a70f1479abc20d4
PhillipHenry <PhillHenry@gmail.com>
2024-01-19 11:46:06 +0000

    Wrapping test (TODO)

--- src/test/java/uk/co/odinconsultants/memory/MemoryMapMainTest.java
+++ src/test/java/uk/co/odinconsultants/memory/MemoryMapMainTest.java
@@ -1,32 +1,51 @@
 package uk.co.odinconsultants.memory;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 
 public class MemoryMapMainTest {
 
+    private final MemoryMapMain toTest =  new MemoryMapMain();
+
     @Test
     public void readFromChannel() {
-        var app = new MemoryMapMain();
         int bufferSize = 32;
         int maxIndex = bufferSize * 5;
         var bytes = new byte[bufferSize];
         ByteBuffer buffer = cyclicMonotonicallyIncreasing(maxIndex);
-        int newIndex = app.bufferIntoArray(maxIndex, buffer, bytes, 0);
+        int newIndex = toTest.bufferIntoArray(maxIndex, buffer, bytes, 0);
         assertEquals(bufferSize, newIndex);
         checkBytes(bytes, 0);
-        int finalIndex = app.bufferIntoArray(maxIndex, buffer, bytes, newIndex);
+        int finalIndex = toTest.bufferIntoArray(maxIndex, buffer, bytes, newIndex);
         checkBytes(bytes, newIndex);
         assertEquals(bufferSize * 2, finalIndex);
+    }
+
+    @Test
+    public void wrapReading() {
+        int bufferSize = 32;
+        int maxIndex = (bufferSize * 5);
+        var bytes = new byte[bufferSize];
+        ByteBuffer buffer = cyclicMonotonicallyIncreasing(maxIndex);
+        int start = 0;
+        for (int i = 0 ; i < maxIndex ; i += bufferSize) {
+            int newIndex = toTest.bufferIntoArray(maxIndex, buffer, bytes, start);
+            System.out.println(String.format("%d\t%d", start, newIndex));
+            checkBytes(bytes, start);
+            start = newIndex;
+        }
     }
 
     private void checkBytes(byte[] bytes, int start) {
         byte b = (byte) (start % 256);
         for (int i = 0; i < bytes.length; i++) {
-            assertEquals(b, bytes[i]);
+            MatcherAssert.assertThat("index = " + i, bytes[i], is(equalTo(b)));
             b += 1;
         }
     }
