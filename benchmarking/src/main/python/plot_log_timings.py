43a8e0a3fd8b93270333399c74bcddfe8d3dd005
PhillipHenry <PhillHenry@gmail.com>
2024-03-09 10:22:18 +0000

    Added memory. Plotting

---
+++ benchmarking/src/main/python/plot_log_timings.py
@@ -0,0 +1,50 @@
+import sys
+import re
+import math
+import matplotlib.pyplot as plt
+
+class Result:
+    def __init__(self, x, value, error):
+        self.x = x
+        self.value = value
+        self.error = error
+
+    def __repr__(self):
+        return f"{self.x},{self.value},{self.error}"
+
+    def __str__(self):
+        return f"{self.x},{self.value},{self.error}"
+
+
+def read(logs):
+    results = []
+    with open(logs, "r") as f:
+        for line in f.readlines():
+            line = line.rstrip()
+            elements = line.split(" ")
+            n = elements[0]
+            value = elements[1]
+            error = elements[2]
+            results.append(Result(n, value, error))
+    return results
+
+
+def plot_results(results: [Result]):
+    print(results)
+    xs_log = [math.log10(int(result.x)) for result in results]
+    errors_log = [float(result.error) for result in results]
+    ys = [float(result.value) for result in results]
+    print(list(zip(xs_log, ys)))
+    # plt.plot(xs_log, ys)
+    plt.errorbar(xs_log, ys, yerr=errors_log, capsize=5, fmt="r--.", ecolor = "black")
+    plt.xlabel("Array size (log10)")
+    plt.ylabel("Ops/second (10 millions)")
+    plt.title("Array access throughput")
+    plt.show()
+
+
+if __name__ == "__main__":
+    logs = sys.argv[1]
+    print(f"Opening file {logs}")
+    results = read(logs)
+    plot_results(results)
